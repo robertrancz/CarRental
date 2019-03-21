bad2f83912f5ee0d9b1405b9dbebf2ade0430085
robertrancz <rancz.robi@gmail.com>
2019-03-21 21:46:10 +0200

    Create the data repository factory.

--- CarRental.Data.Tests/DataLayerTests.cs
+++ CarRental.Data.Tests/DataLayerTests.cs
@@ -29,6 +29,14 @@ namespace CarRental.Data.Tests
         }
 
         [TestMethod]
+        public void test_repository_factory_usage()
+        {
+            var repositoryFactoryTest = new RepositoryFactoryTestClass();
+            IEnumerable<Car> cars = repositoryFactoryTest.GetCars();
+            Assert.IsNotNull(cars);
+        }
+
+        [TestMethod]
         public void test_repository_mocking()
         {
             var expectedCars = new List<Car>
@@ -47,6 +55,48 @@ namespace CarRental.Data.Tests
 
             Assert.AreEqual(expectedCars, actualCars);
         }
+
+        [TestMethod]
+        public void test_factory_mocking1()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car() { CarId = 1, Description = "Mustang" },
+                new Car() { CarId = 2, Description = "Corvette" }
+            };
+
+            Mock<IDataRepositoryFactory> mockDataRepository = new Mock<IDataRepositoryFactory>();
+            mockDataRepository.Setup(obj => obj.GetDataRepository<ICarRepository>().Get()).Returns(cars);
+
+            RepositoryFactoryTestClass factoryTest = new RepositoryFactoryTestClass(mockDataRepository.Object);
+
+            IEnumerable<Car> ret = factoryTest.GetCars();
+
+            Assert.IsTrue(ret == cars);
+        }
+
+        [TestMethod]
+        public void test_factory_mocking2()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car() { CarId = 1, Description = "Mustang" },
+                new Car() { CarId = 2, Description = "Corvette" }
+            };
+
+            Mock<ICarRepository> mockCarRepository = new Mock<ICarRepository>();
+            mockCarRepository.Setup(obj => obj.Get()).Returns(cars);
+
+            Mock<IDataRepositoryFactory> mockDataRepository = new Mock<IDataRepositoryFactory>();
+            mockDataRepository.Setup(obj => obj.GetDataRepository<ICarRepository>()).Returns(mockCarRepository.Object);
+
+            RepositoryFactoryTestClass factoryTest = new RepositoryFactoryTestClass(mockDataRepository.Object);
+
+            IEnumerable<Car> ret = factoryTest.GetCars();
+
+            Assert.IsTrue(ret == cars);
+        }
+
     }
 
     public class RepositoryTestClass
@@ -67,6 +117,31 @@ namespace CarRental.Data.Tests
         public IEnumerable<Car> GetCars()
         {
             IEnumerable<Car> cars = _carRepository.Get();
+            return cars;
+        }
+    }
+
+    public class RepositoryFactoryTestClass
+    {
+        [Import]
+        private IDataRepositoryFactory _dataRepositoryFactory;
+
+        public RepositoryFactoryTestClass()
+        {
+            ObjectBase.Container.SatisfyImportsOnce(this);
+        }
+
+        public RepositoryFactoryTestClass(IDataRepositoryFactory dataRepositoryFactory)
+        {
+            _dataRepositoryFactory = dataRepositoryFactory;
+        }
+
+        public IEnumerable<Car> GetCars()
+        {
+            ICarRepository carRepository = _dataRepositoryFactory.GetDataRepository<ICarRepository>();
+
+            IEnumerable<Car> cars = carRepository.Get();
+
             return cars;
         }
     }
